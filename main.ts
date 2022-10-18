ef3cd34df8b74019ca379cd0b356fa99bedae182
KonstTheoGR <60752156+konsttheogr@users.noreply.github.com>
2022-10-18 18:16:54 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    xs[0] -= 1
+if (xs[0] == -1) {
+        xs[0] = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    xs[0] += 1
+if (xs[0] == 5) {
+        xs[0] = 4
+    }
+})
+let xs: number[] = []
+xs = [
+2,
+2,
+2,
+2,
+2
+]
+let speed = 1
+basic.forever(function () {
+    basic.clearScreen()
+    led.plot(xs[0], 0)
+    led.plot(xs[1], 1)
+    led.plot(xs[2], 2)
+    led.plot(xs[3], 3)
+    led.plot(xs[4], 4)
+    xs[4] = xs[3]
+    xs[3] = xs[2]
+    xs[2] = xs[1]
+    xs[1] = xs[0]
+    basic.pause(speed * 50)
+})
